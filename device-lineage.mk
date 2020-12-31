1b1937462bc12d5fe9a80b9b83002410f8782eb6
Bruno Martins <bgcngm@gmail.com>
2020-12-31 00:46:08 +0000

    marlin: Use CodeAurora ImsService implementation for RCS
    There is no evidence that Qualcomm devices running a rather recent IMS
    stack need to include AOSP RCS packages. RCS already worked before this
    change and continues to do so, but the main goal was to get rid of some
    of the RCS related log spam, which was accomplished here.
    
    Change-Id: I219546e29d288373877e9bcd194a60ef5e09f224

--- device-lineage.mk
+++ device-lineage.mk
@@ -22,12 +22,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
 # Google Assistant
 PRODUCT_PRODUCT_PROPERTIES += ro.opa.eligible_device=true
 
-# IMS
-PRODUCT_PACKAGES += \
-    com.android.ims.rcsmanager \
-    RcsService \
-    PresencePolling
-
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += device/google/marlin/overlay-lineage
 
