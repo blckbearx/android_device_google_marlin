55db1659a6972f12e42101f13a87d0679133aefb
blckbearx <nachogsr@gmail.com>
2022-01-17 22:13:10 -0300

    marlin/sailfish: Add ih8sn configs from https://github.com/npjohnson/android_vendor_extra.

--- device-lineage.mk
+++ device-lineage.mk
@@ -8,3 +8,11 @@ DEVICE_PACKAGE_OVERLAYS += device/google/marlin/overlay-lineage
 # Trust HAL
 PRODUCT_PACKAGES += \
     vendor.lineage.trust@1.0-service
+
+# ih8sn
+PRODUCT_PACKAGES += ih8sn
+
+ifneq ("$(wildcard  device/google/marlin/ih8sn_configs/ih8sn_$(subst lineage_,,$(TARGET_PRODUCT)).conf)","")
+PRODUCT_COPY_FILES += \
+    device/google/marlin/ih8sn_configs/ih8sn_$(subst lineage_,,$(TARGET_PRODUCT)).conf:/system/etc/ih8sn.conf
+endif
