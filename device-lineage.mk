0464e5d8502b9c722e37a50693825a0295d83180
blckbearx <nachogsr@gmail.com>
2022-02-05 14:48:36 -0300

    marlin/sailfish: Include GApps build option by setting INCLUDE_GAPPS to true and only build ih8sn when building GApps variant.

--- device-lineage.mk
+++ device-lineage.mk
@@ -9,10 +9,15 @@ DEVICE_PACKAGE_OVERLAYS += device/google/marlin/overlay-lineage
 PRODUCT_PACKAGES += \
     vendor.lineage.trust@1.0-service
 
+# crDroid GApps
+ifeq ($(INCLUDE_GAPPS), true)
 # ih8sn
 PRODUCT_PACKAGES += ih8sn
 
 ifneq ("$(wildcard  device/google/marlin/ih8sn_configs/ih8sn_$(subst lineage_,,$(TARGET_PRODUCT)).conf)","")
 PRODUCT_COPY_FILES += \
     device/google/marlin/ih8sn_configs/ih8sn_$(subst lineage_,,$(TARGET_PRODUCT)).conf:/system/etc/ih8sn.conf
+endif
+
+$(call inherit-product-if-exists, vendor/gapps/basic/config.mk)
 endif
