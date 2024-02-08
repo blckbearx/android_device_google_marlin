57be143b639f49328ba289ffc6b39a474a582f5b
blckbearx <nachogsr@gmail.com>
2024-02-08 09:55:17 -0300

    m1s1: PixelBuilds bringup

--- pb_sailfish.mk
+++ pb_sailfish.mk
@@ -2,16 +2,16 @@
 TARGET_SCREEN_HEIGHT := 1920
 TARGET_SCREEN_WIDTH := 1080
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common PixelBuilds stuff.
+$(call inherit-product, vendor/pb/config/common_full_phone.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/google/marlin/aosp_sailfish.mk)
 
--include device/google/marlin/device-lineage.mk
+-include device/google/marlin/device-pb.mk
 
 ## Device identifier. This must come after all inclusions
-PRODUCT_NAME := lineage_sailfish
+PRODUCT_NAME := aosp_sailfish
 PRODUCT_BRAND := google
 PRODUCT_MODEL := Pixel
 TARGET_MANUFACTURER := HTC
