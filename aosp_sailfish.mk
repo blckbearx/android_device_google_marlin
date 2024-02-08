46b81e0a3b807c48abedc31a378cb4cfb3a310aa
blckbearx <nachogsr@gmail.com>
2024-02-08 09:55:17 -0300

    m1s1: PixelBuilds bringup

--- aosp_sailfish.mk
+++ aosp_sailfish.mk
@@ -17,16 +17,31 @@
 # Sample: This is where we'd set a backup provider if we had one
 # $(call inherit-product, device/sample/products/backup_overlay.mk)
 
+# Boot animation
+TARGET_SCREEN_HEIGHT := 1920
+TARGET_SCREEN_WIDTH := 1080
+
+# Inherit some common PixelBuilds stuff.
+$(call inherit-product, vendor/pb/config/common_full_phone.mk)
+
 # Inherit from the common Open Source product configuration
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
 
+## Device identifier. This must come after all inclusions
 PRODUCT_NAME := aosp_sailfish
 PRODUCT_DEVICE := sailfish
-PRODUCT_BRAND := Android
-PRODUCT_MODEL := AOSP on msm8996
+PRODUCT_BRAND := google
+PRODUCT_MODEL := Pixel
 PRODUCT_MANUFACTURER := Google
-PRODUCT_RESTRICT_VENDOR_FILES := true
+TARGET_MANUFACTURER := HTC
+PRODUCT_RESTRICT_VENDOR_FILES := false
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRODUCT_NAME=sailfish \
+    PRIVATE_BUILD_DESC="sailfish-user 10 QP1A.191005.007.A3 5972272 release-keys"
+
+BUILD_FINGERPRINT := google/sailfish/sailfish:10/QP1A.191005.007.A3/5972272:user/release-keys
 
 PRODUCT_COPY_FILES += device/google/marlin/fstab.common:$(TARGET_COPY_OUT_VENDOR)/etc/fstab.sailfish
 PRODUCT_COPY_FILES += device/google/marlin/fstab.common:$(TARGET_COPY_OUT_RECOVERY)/root/first_stage_ramdisk/fstab.sailfish
@@ -37,4 +52,8 @@ $(call inherit-product-if-exists, vendor/google_devices/marlin/device-vendor-sai
 PRODUCT_PACKAGES += \
     Launcher3QuickStep \
     WallpaperPicker
+
+-include device/google/marlin/device-pb.mk
+
+$(call inherit-product, vendor/google/sailfish/sailfish-vendor.mk)
 
