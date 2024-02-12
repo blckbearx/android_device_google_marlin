80df8b39da85330100b1837b49ba029c5da2e95e
blckbearx <nachogsr@gmail.com>
2024-02-12 13:20:45 -0300

    m1s1: Merge device makefiles.

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,8 +1,6 @@
 PRODUCT_MAKEFILES := \
 	$(LOCAL_DIR)/aosp_marlin.mk \
 	$(LOCAL_DIR)/aosp_sailfish.mk \
-	$(LOCAL_DIR)/pb_marlin.mk \
-	$(LOCAL_DIR)/pb_sailfish.mk
 
 COMMON_LUNCH_CHOICES := \
 	aosp_marlin-userdebug \
--- aosp_marlin.mk
+++ aosp_marlin.mk
@@ -17,16 +17,31 @@
 # Sample: This is where we'd set a backup provider if we had one
 # $(call inherit-product, device/sample/products/backup_overlay.mk)
 
+# Boot animation
+TARGET_SCREEN_HEIGHT := 2560
+TARGET_SCREEN_WIDTH := 1440
+
+# Inherit some common PixelBuilds stuff.
+$(call inherit-product, vendor/pb/config/common_full_phone.mk)
+
 # Inherit from the common Open Source product configuration
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
 
+## Device identifier. This must come after all inclusions
 PRODUCT_NAME := aosp_marlin
 PRODUCT_DEVICE := marlin
-PRODUCT_BRAND := Android
-PRODUCT_MODEL := AOSP on msm8996
+PRODUCT_BRAND := google
+PRODUCT_MODEL := Pixel XL
 PRODUCT_MANUFACTURER := Google
-PRODUCT_RESTRICT_VENDOR_FILES := true
+TARGET_MANUFACTURER := HTC
+PRODUCT_RESTRICT_VENDOR_FILES := false
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRODUCT_NAME=marlin \
+    PRIVATE_BUILD_DESC="marlin-user 10 QP1A.191005.007.A3 5972272 release-keys"
+
+BUILD_FINGERPRINT := google/marlin/marlin:10/QP1A.191005.007.A3/5972272:user/release-keys
 
 PRODUCT_COPY_FILES += device/google/marlin/fstab.common:$(TARGET_COPY_OUT_VENDOR)/etc/fstab.marlin
 PRODUCT_COPY_FILES += device/google/marlin/fstab.common:$(TARGET_COPY_OUT_RECOVERY)/root/first_stage_ramdisk/etc/fstab.marlin
@@ -38,3 +53,6 @@ PRODUCT_PACKAGES += \
     Launcher3QuickStep \
     WallpaperPicker
 
+-include device/google/marlin/device-pb.mk
+
+$(call inherit-product, vendor/google/marlin/marlin-vendor.mk)
